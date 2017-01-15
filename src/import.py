84c03f591b555b1f43792af590bd750df1a26c44
Joe Lin <joelin000@gmail.com>
2017-01-15 17:16:16 +0800

    Finish all importing features and add readme.

--- src/import.py
+++ src/import.py
@@ -30,16 +30,19 @@ def get_notes(task_id, data):
     notes = []
     for n in data['notes']:
         if n['task_id'] == task_id:
+            # remove unnecessary info
             del n['created_by_request_id']
             del n['revision']
             del n['type']
+
             notes.append(n)
-    return notes if notes != [] else None
+    return notes
 
 
 def get_reminder(task_id, data):
     for r in data['reminders']:
         if r['task_id'] == task_id:
+            # remove unnecessary info
             del r['updated_at']
             del r['created_at']
             del r['created_by_request_id']
@@ -53,7 +56,7 @@ def get_subtasks(task_id, data):
     subtasks = []
     for sub in data['subtasks']:
         if sub['task_id'] == task_id:
-            # remove unneeded info
+            # remove unnecessary info
             if sub['completed'] is True:
                 del sub['completed_at']
             del sub['created_at']
@@ -63,8 +66,7 @@ def get_subtasks(task_id, data):
             del sub['type']
 
             subtasks.append(sub)
-
-    return subtasks if subtasks != [] else None
+    return subtasks
 
 
 def get_tasks(list_id, data):
@@ -76,7 +78,7 @@ def get_tasks(list_id, data):
             t['reminder'] = get_reminder(t['id'], data)
             t['subtasks'] = get_subtasks(t['id'], data)
 
-            # remove unneeded info
+            # remove unnecessary info
             if t['completed'] is True:
                 del t['completed_by_id']
                 del t['completed_at']
@@ -87,8 +89,14 @@ def get_tasks(list_id, data):
             del t['type']
 
             tasks.append(t)
+    return tasks
 
-    return tasks if tasks != [] else None
+
+def update_item_orders(begin_order):
+    """Update tasks' order that are greater than `begin_order`."""
+    for task in t_tasks.values():
+        if task['item_order'] >= begin_order:
+            api.items.get_by_id(task['id']).update(item_order=task['item_order']+1)
 
 
 def read_wunderlist_data(data_file):
@@ -104,8 +112,20 @@ def read_wunderlist_data(data_file):
             'title': list['title'],
             'tasks': get_tasks(list['id'], data)
         }
-
     return c_lists
+
+
+def write_json_to_file(obj_list, dest_file):
+    data = []
+    for o in obj_list:
+        tmp = {}
+        for col in ['id', 'name']:
+            tmp[col] = o[col]
+        data.append(tmp)
+
+    with open(dest_file, 'a') as f:
+        json.dump(data, f)
+        f.write('\n')
 
 
 if __name__ == '__main__':
@@ -114,30 +134,57 @@ if __name__ == '__main__':
     wunderlist_export_file = "../data/test_wunderlist.json"
 
     api = get_authed_api_session(auth_file)
-    lists = read_wunderlist_data(wunderlist_export_file)
-    pp(lists)
+    w_lists = read_wunderlist_data(wunderlist_export_file)
 
+    # Create a root project for containing lists from Wunderlist
+    root_project = api.projects.add('Imported from Wunderlist')
 
+    api.commit()
 
-#+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=#
-#           Todoist APIs             #
-#+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=#
+    # Get the item order of the next project
+    order = root_project['item_order'] + 1
 
-#--- https://developer.todoist.com ---#
+    # Create projects from lists in Wunderlist
+    t_projects = {}
+    for list in w_lists.values():
+        t_projects[list['id']] = api.projects.add(list['title'], indent=2, item_order=order)
+        order += 1
 
-### Project APIs
-# api.projects.add(, item_order=) // add a new project
-# api.projects.get_by_id(128501815).update(options...) // update a project's arguments
-# api.projects.get_by_id(128501815).delete() // delete a project
-# api.projects.get_by_id(128501815).archive() // delete a project
-# api.projects.get_by_id(128501815).unarchive() // delete a project
-# api.projects.update_orders_indents({128501470: [42, 1], 128501607: [43, 1]}) // project_id: [item_order, indent]
-# api.templates.import_into_project(128501470, 'example.csv') // import data
-# api.templates.export_as_file(128501470) // export as a file
-# api.templates.export_as_url(128501470) // export as an url
+    api.commit()
 
-### Item APIs
-# api.items.add('Task1', 128501470)
+    # Add tasks to Todoist
+    t_tasks = {}
+    for list in w_lists.values():
+        for t in list['tasks']:
+            pri = 1 if t['starred'] is False else 2
+            if t['completed'] == False: # to ignore completed tasks
+                t_tasks[t['id']] = api.items.add(t['title'], t_projects[list['id']]['id'], priority=pri)
+            else:
+                t_tasks[t['id']] = api.items.add(t['title'], t_projects[list['id']]['id'], priority=pri, checked=1)
 
+    api.commit()
 
-# api.commit() // commit changes made above
+    # Add subtasks, reminder and notes to tasks
+    for list in w_lists.values():
+        for t in list['tasks']:
+            # Add subtasks
+            order = t_tasks[t['id']]['item_order'] + 1
+            for sub in t['subtasks']:
+                if sub['completed'] == False:  # to ignore completed tasks
+                    tmp_sub = api.items.add(sub['title'], t_projects[list['id']]['id'], indent=2, item_order=order)
+                else:
+                    tmp_sub = api.items.add(sub['title'], t_projects[list['id']]['id'], indent=2, item_order=order, checked=1)
+
+                update_item_orders(order)
+                t_tasks[sub['id']] = tmp_sub
+                order += 1
+
+            # Add reminder
+            if t['reminder'] != None:
+                api.reminders.add(t_tasks[t['id']]['id'], service='email', date_string=t['reminder']['date'])
+
+            # Add notes
+            for note in t['notes']:
+                api.notes.add(t_tasks[t['id']]['id'], note['content'])
+
+    api.commit()
